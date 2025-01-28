935593c465ac070f4deff92f51f3a02225ad693b
Fatema Afroza <fafroza2017@gmail.com>
2025-01-28 09:39:58 +0100

    Loggar ingredienser & instruktioner

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 let userInput = document.getElementById("userInput").value;
 
-fetch(url + "pizza")
+fetch(url + "big mac")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        //console.log(data);
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInstructions);      
+        let count = 1;
+        let ingredient = [];
+        for (let i in myMeal) {
+            let ingredient = "";
+            let measure = "";
+            if (i.startsWith("strIngredient") && myMeal[i]) {
+                ingredient = myMeal[i];
+                measure = myMeal[`strMeasure` + count];
+                count += 1;
+                console.log(ingredient, measure);
+                
+            }
+        }
         
     });
