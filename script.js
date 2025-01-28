8d9540013f3ce0e6dbef3a503d83b56a51005004
Fatema Afroza <fafroza2017@gmail.com>
2025-01-28 08:52:11 +0100

    Skapat variables i js som tar emot html element

---
+++ script.js
@@ -0,0 +1,6 @@
+//Variables som tar imot html element
+let searchBtn = document.getElementById("searchBtn");
+let result = document.getElementById("result");
+let url = "www.themealdb.com/api/json/v1/1/search.php?s=";
+
+let userInput = document.getElementById("userInput");
