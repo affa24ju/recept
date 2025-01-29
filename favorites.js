ebd527edb5a6e5077b47608559da97ecf7c92acf
Fatema Afroza <fafroza2017@gmail.com>
2025-01-29 11:22:57 +0100

    Skapat favriter html & script sida

---
+++ favorites.js
@@ -0,0 +1,17 @@
+//Backend API url
+const apiUrl ="http://localhost:8080/api/favorites";
+
+//Funktion för att lägga till som favorit
+function addFavorit(meal){
+
+}
+
+//Funktion för att visa alla favoriter
+function showFavorites(){
+
+}
+
+//Funktion för att ta bort en favorit
+function deleteFavorit(){
+
+}
